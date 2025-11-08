8a6fa90caa81d6f4b0f771366141066494ac8dcb
Karol Wojtas <developer.mdobska@gmail.com>
2025-11-08 21:43:37 +0100

    initial anim

--- app/routes/home/components/settings-float.tsx
+++ app/routes/home/components/settings-float.tsx
@@ -1,9 +1,18 @@
 import { IconButton, Portal, useBreakpointValue, VStack } from '@chakra-ui/react';
-import { useState } from 'react';
+import { AnimatePresence, LayoutGroup, motion } from 'motion/react';
+import { useState, type PropsWithChildren } from 'react';
 import { useTranslation } from 'react-i18next';
 import { LuEllipsisVertical, LuMoon, LuSun, LuX } from 'react-icons/lu';
 import { useColorMode } from '~/components/ui/color-mode';
 import { LangSelect } from './lang-select';
+
+const AnimatedSettingsItem = ({ children }: PropsWithChildren) => {
+    return (
+        <motion.div layout initial={{ translateX: '300%' }} animate={{ translateX: 0 }} exit={{ translateX: '300%' }}>
+            {children}
+        </motion.div>
+    );
+};
 
 export const SettingsFloat = () => {
     const { t } = useTranslation();
@@ -13,22 +22,40 @@ export const SettingsFloat = () => {
     return (
         <Portal>
             <div style={{ position: 'fixed', bottom: offset, right: offset }}>
-                <VStack align="flex-end">
-                    <IconButton
-                        aria-label={t('Search')}
-                        variant="subtle"
-                        size={{ base: 'md' }}
-                        onClick={() => setExpanded(!expanded)}
-                    >
-                        {expanded ? <LuX /> : <LuEllipsisVertical />}
-                    </IconButton>
-                    {expanded && (
-                        <IconButton aria-label="Switch color mode" onClick={() => toggleColorMode()} variant="subtle">
-                            {colorMode === 'light' ? <LuMoon /> : <LuSun />}
-                        </IconButton>
-                    )}
-                    {expanded && <LangSelect />}
-                </VStack>
+                <LayoutGroup>
+                    <VStack align="flex-end">
+                        <AnimatePresence mode="popLayout">
+                            {expanded && (
+                                <AnimatedSettingsItem>
+                                    <IconButton
+                                        aria-label="Switch color mode"
+                                        onClick={() => toggleColorMode()}
+                                        variant="subtle"
+                                    >
+                                        {colorMode === 'light' ? <LuMoon /> : <LuSun />}
+                                    </IconButton>
+                                </AnimatedSettingsItem>
+                            )}
+                        </AnimatePresence>
+                        <AnimatePresence>
+                            {expanded && (
+                                <AnimatedSettingsItem>
+                                    <LangSelect />
+                                </AnimatedSettingsItem>
+                            )}
+                        </AnimatePresence>
+                        <motion.div layout>
+                            <IconButton
+                                aria-label={t('Search')}
+                                variant="subtle"
+                                size={{ base: 'md' }}
+                                onClick={() => setExpanded(!expanded)}
+                            >
+                                {expanded ? <LuX /> : <LuEllipsisVertical />}
+                            </IconButton>
+                        </motion.div>
+                    </VStack>
+                </LayoutGroup>
             </div>
         </Portal>
     );
